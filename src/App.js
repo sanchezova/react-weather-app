544b1df17bc4df9ddfca49f15e93e3ecbc4a44f1
Ivana <118789299+sanchezova@users.noreply.github.com>
2023-03-05 19:54:57 -0800

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Ivana Pichova and is{" "}
+        <a
+          href="https://github.com/sanchezova/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
