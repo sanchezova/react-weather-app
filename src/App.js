fd006e00ca0f751abc64d762dea6425c091c97bd
Ivana <118789299+sanchezova@users.noreply.github.com>
2023-03-05 21:53:12 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Ivana Pichova and is{" "}
-        <a
-          href="https://github.com/sanchezova/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Ivana Pichova and is{" "}
+          <a
+            href="https://github.com/sanchezova/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
