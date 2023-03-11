8070a4a83a937ec29d590135017b128dad8bae9c
Ivana <118789299+sanchezova@users.noreply.github.com>
2023-03-10 17:18:20 -0800

    Created new component WeatherTemperature.js and displayed relevant temperature in C and F

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,10 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <WeatherIcon code={props.data.icon} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">℃</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
 
         <div className="col-6 mt-3">
